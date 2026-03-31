76918b7abe1c0ea5cf517e672ff6e33ee21a3118
ChaseBP <praveen.27csb@licet.ac.in>
2026-04-01 04:58:28 +0530

    Test GitSCM

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 	@GetMapping("/")
 	public String hello() {
-		return "Hello, Nice to meet ya!";
+		return "Hello, Nice to meet ya!, Konnichiwa";
 	}
 }
